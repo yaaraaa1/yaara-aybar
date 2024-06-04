60ad751786e7ea558559f2afcb7d640874097824
Yaara Aybar <yaara.aybar@yale.edu>
2024-06-04 01:25:27 -0400

    Trying to fix image resize

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ window.onload = function() {
     const areas = map.getElementsByTagName('area');
 
     const originalWidth = 1600; // Original width of the image
-    const originalHeight = 1200; // Original height of the image (if known)
+    const originalHeight = 1200; // Original height of the image
 
     function resizeMap() {
         const widthRatio = img.clientWidth / originalWidth;
@@ -23,4 +23,20 @@ window.onload = function() {
 
     window.addEventListener('resize', resizeMap);
     resizeMap();
+
+    // Handle hover events
+    for (let i = 0; i < areas.length; i++) {
+        areas[i].addEventListener('mouseover', function() {
+            const targetId = this.getAttribute('data-hover-target');
+            if (targetId) {
+                document.getElementById(targetId).style.display = 'block';
+            }
+        });
+        areas[i].addEventListener('mouseout', function() {
+            const targetId = this.getAttribute('data-hover-target');
+            if (targetId) {
+                document.getElementById(targetId).style.display = 'none';
+            }
+        });
+    }
 };
